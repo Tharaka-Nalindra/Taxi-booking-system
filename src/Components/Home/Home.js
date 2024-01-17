d6a209971c885bc5b24775003fa440f476d3e4ae
Tharaka-Nalindra <124589042+Tharaka-Nalindra@users.noreply.github.com>
2024-01-17 20:29:51 +0530

    Update Home.js

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "./HomeModule.css";
-// import pagecar from "../image/pagecar.png";
 import { Link } from "react-router-dom";
 import Nav from '../Navbar/Nav';
 
@@ -8,12 +7,6 @@ export default function TaxiBooking(){
     return (
         <div>
             <div><Nav/></div>
-
-            
-
-            {/* <div className="PageCar">
-                <img src={pagecar} alt="car"></img>
-            </div> */}
 
             <div className="welcome">
                 <h1><Link to="/CoustomerInput">Book Now</Link></h1>
