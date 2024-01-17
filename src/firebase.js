729695d4b6d53a035aeaeecfc05eef134e1adf78
Tharaka-Nalindra <124589042+Tharaka-Nalindra@users.noreply.github.com>
2024-01-17 20:37:00 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import {getAuth} from 'firebase/auth';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBbIrN20oMhVika7XyH-uYtONLJkIIxVuM",
   authDomain: "taxi-booking-15bc6.firebaseapp.com",
