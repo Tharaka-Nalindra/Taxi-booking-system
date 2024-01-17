a3fc6bd71e16eb9c413c72190dedb5ecdba711c0
Tharaka-Nalindra <124589042+Tharaka-Nalindra@users.noreply.github.com>
2024-01-17 20:21:51 +0530

    Update AdminDashboad.js

--- src/Components/AdminDashboad/AdminDashboad.js
+++ src/Components/AdminDashboad/AdminDashboad.js
@@ -1,8 +1,6 @@
 import React from "react";
 import "./AdminDashboadModule.css";
-// import pagecar from "../image/pagecar.png";
 import logo from '../image/logo.png'
-// import { Link } from "react-router-dom";
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
